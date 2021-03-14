c72126fbbba936a97865af97bac34f865c1a7c95
clouddeveer <developer0722@qq.com>
2021-03-15 00:21:56 +0800

    全局Logs

--- devops_api/src/main.ts
+++ devops_api/src/main.ts
@@ -3,7 +3,9 @@ import { ApplicationModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const port = process.env.PORT || 8080;
-
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exception/any-exceptions.filter';
+import { HttpExceptionsFilter } from './common/exception/http-exceptions.filer';
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
 
@@ -20,6 +22,13 @@ async function bootstrap() {
   // 设置 swagger 网址
   SwaggerModule.setup('docs', app, document);
   app.useGlobalPipes(new ValidationPipe()); //开启一个全局验证管道
+
+  // 使用全局拦截器打印出参
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 所有异常
+  app.useGlobalFilters(new AllExceptionsFilter());
+  // http 异常
+  app.useGlobalFilters(new HttpExceptionsFilter());
   await app.listen(port);
   Logger.log(`http://localhost:${port}`, '服务启动成功');
 }
